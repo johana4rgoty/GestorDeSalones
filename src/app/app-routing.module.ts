e8293a053b994d22320dffa1fa27fc8b28853696
AndresGJ144 <andres_sgj144@unicauca.edu.co>
2020-02-11 11:55:47 -0500

    Creacion de compoentes de login-coordinador-home
    Autor:Andres Garzon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  
+  {
+    path: 'coordinador', loadChildren: () => import('./components/coordinador/coordinador.module').then(m => m.CoordinadorModule) 
+    //path: 'coordinador', component:CoordinadorComponent 
+  },
+  { 
+    path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) 
+    //path: 'login', component:LoginComponent
+  },
+  
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
